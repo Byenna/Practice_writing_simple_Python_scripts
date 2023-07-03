c020d02126fc1e60b045a8f7338c4f3ba58e3142
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-03 13:52:43 +0200

    Putting it all together

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -1,11 +1,18 @@
 # Prompt the user to enter the temperature in Celsius Use the 'input()' function to get the user's input.
 
-celcius = float(input("Please enter the temperature in Celsius: "))
+# celcius = float(input("Please enter the temperature in Celsius: "))
 
 # Convert the temperature to Fahrenheit and store it in a variable called fahrenheit:
 
-farenheit = (celcius * 9/5) + 32
+# farenheit = (celcius * 9/5) + 32
 
 # Print the converted temperature
 
+# print(f"The temperature in Farenheit is: {farenheit} degrees.")
+
+# Putting it all together:
+# write a script that asks the user to enter a temperature in Celsius and converts it to Fahrenheit using the provided formula
+
+celcius = float(input("Please enter the temperature in Celsius: "))
+farenheit = (celcius * 9/5) + 32
 print(f"The temperature in Farenheit is: {farenheit} degrees.")
