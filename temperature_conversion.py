17c5320f5717d8d92e5c77b2f221c6694934b184
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-03 13:38:20 +0200

    Converted and stored temperature

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -1,3 +1,7 @@
 # Prompt the user to enter the temperature in Celsius Use the 'input()' function to get the user's input.
 
 celcius = float(input("Please enter the temperature in Celsius: "))
+
+# Convert the temperature to Fahrenheit and store it in a variable called fahrenheit:
+
+farenheit = (celcius * 9/5) + 32
