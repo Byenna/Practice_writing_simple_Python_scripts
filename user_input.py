13f9a03ece6477f2e3a9228e58ee7de71c825b3e
Byenna <72694655+Byenna@users.noreply.github.com>
2023-07-03 13:20:47 +0200

    putting it all together

--- user_input.py
+++ user_input.py
@@ -1,11 +1,17 @@
 # User input
 
 # Prompt the user to enter their name 
-name = input("Please enter your name: ")
+# name = input("Please enter your name: ")
 
 # Print the greeting message
 #print("Hello " + name + "! Nice to meet you!")
 
 
 # Print the greeting message with f-string
-print(f"Hello, {name}! Nice to meet you!")
+#print(f"Hello, {name}! Nice to meet you!")
+
+# Putting it all together:
+# write a script that asks the user for their name and then prints a greeting message with their name.
+
+name = input("Please enter your name: ")
+print(f"Hello {name}! Nice to meet you")
